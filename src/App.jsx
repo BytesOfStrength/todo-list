b560e41c64132ce7c10be3acf7ddd844c67d0d69
BytesOfStrength <213146959+BytesOfStrength@users.noreply.github.com>
2025-12-14 20:11:23 -0500

    week-05-todos-events: Update functionality of TodoApp ( update state, TodoForm and TodoList components)

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
 
 function App() {
-  const [newTodo, setNewTodo] = useState('test');
+  const [todoList, setTodoList] = useState([]);
+  function addTodo(title) {
+    const newTodo = {
+      title: title,
+      id: Date.now(),
+    };
+    setTodoList([...todoList, newTodo]);
+  }
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm />
-      <p>{newTodo}</p>
-      <TodoList />
+      <TodoForm onAddTodo={addTodo} />
+
+      <TodoList todoList={todoList} />
     </div>
   );
 }
