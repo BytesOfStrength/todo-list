98a632141d717e64fdec50a944fb3b299e4ad4b1
BytesOfStrength <213146959+BytesOfStrength@users.noreply.github.com>
2026-01-13 00:30:06 -0500

    week-09-data-fetch: Add Transition Variables, Add Loading Indicator, Display Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,57 @@
 //week 09 Airtable credential setup
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import TodoList from './features/TodoList/TodoList.jsx';
 import TodoForm from './features/TodoForm.jsx';
 
+const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+const token = `Bearer ${import.meta.env.VITE_PAT}`;
+
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setIsLoading(true);
+      setErrorMessage('');
+      const options = {
+        method: 'GET',
+        headers: {
+          Authorization: token,
+        },
+      };
+      //try/catch/finally
+      try {
+        const resp = await fetch(url, options);
+        if (!resp.ok) {
+          throw new Error(`Error: ${resp.status} ${resp.statusText}`);
+        }
+
+        const { records } = await resp.json();
+        setTodoList(
+          records.map((record) => {
+            const todo = {
+              id: record.id,
+              ...record.fields,
+            };
+            if (!todo.isCompleted) {
+              todo.isCompleted = false;
+            }
+            return todo;
+          })
+        );
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchTodos();
+  }, []);
+
   const addTodo = (title) => {
     const newTodo = {
       title,
@@ -43,7 +89,17 @@ function App() {
         todoList={todoList}
         onCompleteTodo={completeTodo}
         onUpdateTodo={updateTodo}
+        isLoading={isLoading}
       />
+      {errorMessage && (
+        <div>
+          <hr />
+          <p>{errorMessage}</p>
+          <button type="button" onClick={() => setErrorMessage('')}>
+            Dismiss
+          </button>
+        </div>
+      )}
     </div>
   );
 }
